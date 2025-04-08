d022259be088fb8f309b7b9419efc11a3ff00e3e
Adrift01 <kikorso2023@gmail.com>
2025-04-09 03:01:51 +0600

    Update server.js

--- server.js
+++ server.js
@@ -1,57 +1,37 @@
 const express = require('express');
-const http = require('http');
-const socketIo = require('socket.io');
 const app = express();
-const server = http.createServer(app);
-const io = socketIo(server);
-
+const http = require('http').createServer(app);
+const io = require('socket.io')(http);
 const path = require('path');
-const users = {}; // Store user info with socket.id
 
-app.use(express.static(path.join(__dirname, 'public'))); // Serve frontend
+app.use(express.static(path.join(__dirname, '/')));
 
-io.on('connection', (socket) => {
-  console.log('A user connected:', socket.id);
+let onlineUsers = {};
 
-  // When a user joins
-  socket.on('join', (data) => {
-    users[socket.id] = { id: socket.id, user: data.user, pic: data.pic };
-    io.emit('onlineUsers', Object.values(users));
+io.on('connection', socket => {
+  socket.on('join', data => {
+    onlineUsers[socket.id] = { id: socket.id, user: data.user, pic: data.pic };
+    io.emit('onlineUsers', Object.values(onlineUsers));
   });
 
-  // Public message
-  socket.on('message', (msg) => {
-    const timestamp = new Date().toLocaleString();
-    io.emit('message', { ...msg, timestamp });
+  socket.on('message', msg => {
+    io.emit('message', msg);
   });
 
-  // Private message
-  socket.on('privateMessage', (msg) => {
-    const timestamp = new Date().toLocaleString();
-    io.to(msg.to).emit('privateMessage', {
-      user: msg.user,
-      text: msg.text,
-      timestamp
-    });
-
-    // Also show it to the sender
-    socket.emit('privateMessage', {
-      user: msg.user + ' (You)',
-      text: msg.text,
-      timestamp
+  socket.on('privateMessage', data => {
+    io.to(data.to).emit('privateMessage', {
+      user: data.user,
+      text: data.text
     });
   });
 
-  // Disconnect
   socket.on('disconnect', () => {
-    delete users[socket.id];
-    io.emit('onlineUsers', Object.values(users));
-    console.log('A user disconnected:', socket.id);
+    delete onlineUsers[socket.id];
+    io.emit('onlineUsers', Object.values(onlineUsers));
   });
 });
 
-// Start server
 const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+http.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
