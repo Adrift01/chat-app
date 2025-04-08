487a35d1c8c42b51ba7f333a7528f423c9cbe721
Adrift01 <kikorso2023@gmail.com>
2025-04-09 02:08:05 +0600

    Update server.js

--- server.js
+++ server.js
@@ -6,29 +6,32 @@ const path = require('path');
 
 app.use(express.static(path.join(__dirname, '/')));
 
-let users = [];
+let onlineUsers = {};
 
-io.on('connection', (socket) => {
-  console.log('A user connected');
-
-  socket.on('join', (userData) => {
-    socket.userData = userData;
-    users.push({ id: socket.id, ...userData });
-    io.emit('onlineUsers', users);
+io.on('connection', socket => {
+  socket.on('join', data => {
+    onlineUsers[socket.id] = { id: socket.id, user: data.user, pic: data.pic };
+    io.emit('onlineUsers', Object.values(onlineUsers));
   });
 
-  socket.on('message', (msg) => {
+  socket.on('message', msg => {
     io.emit('message', msg);
   });
 
+  socket.on('privateMessage', data => {
+    io.to(data.to).emit('privateMessage', {
+      user: data.user,
+      text: data.text
+    });
+  });
+
   socket.on('disconnect', () => {
-    console.log('A user disconnected');
-    users = users.filter(u => u.id !== socket.id);
-    io.emit('onlineUsers', users);
+    delete onlineUsers[socket.id];
+    io.emit('onlineUsers', Object.values(onlineUsers));
   });
 });
 
 const PORT = process.env.PORT || 3000;
 http.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
