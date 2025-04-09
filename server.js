75ce9bdd7a102d11b97ead81ecb2973ca5f1ef1c
Adrift01 <kikorso2023@gmail.com>
2025-04-09 20:12:34 +0600

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const path = require('path');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 let onlineUsers = {};
